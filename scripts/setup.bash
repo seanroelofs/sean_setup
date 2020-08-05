b72e34d4a7dd8c3c402c658db3b9027cbac90692
Sean Roelofs <sean.forrester.roelofs@gmail.com>
2020-08-05 14:36:22 -0700

    Add keyboard and git to main setup script

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -3,4 +3,6 @@ sudo apt-get update
 bash ~/sean_setup/scripts/vim.bash
 bash ~/sean_setup/scripts/tmux.bash
 bash ~/sean_setup/scripts/trash.bash
+bash ~/sean_setup/scripts/keyboard.bash
+bash ~/sean_setup/scripts/git.bash
 
